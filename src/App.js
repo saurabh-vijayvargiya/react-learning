0581d41f36c2b4919b1d28fef3c6cf0049d8915a
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2018-10-14 11:25:00 +0530

    Learned about setting state

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  increaseCounter() {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  decreaseCounter() {
+    this.setState({
+      counter: this.state.counter - 1
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React for fun
-          </a>
+          <p>{this.state.counter}</p>
+          <button onClick={this.increaseCounter.bind(this)}>Increase</button>
+          <button onClick={this.decreaseCounter.bind(this)}>Decrease</button>
         </header>
       </div>
     );
