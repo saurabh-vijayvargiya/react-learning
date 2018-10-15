6c3d775a3f7672acc5bea5d9c51966f2e8e4290c
Saurabh Vijayvargiya <saurabhvv0808@gmail.com>
2018-10-15 19:31:55 +0530

    printing data fetched from get api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      loading: true,
+      data: null
+    }
+    this.loadingData();
+  }
+
+  loadingData = () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(json => {
+      this.setState({
+        loading: false,
+        data: json
+      });
+    });
+  }
+
+  customRender = () => {
+    if(this.state.loading) {
+      return (
+        <p>Loading ... </p>
+      );
+    } else {
+      let data = this.state.data;
+      let output = data.map(d => {
+        return (
+          <div key={d.id}>
+            <p>{d.name} &nbsp; {d.email}</p>
+          </div>
+        );
+      });
+
+      return output;
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React for fun
-          </a>
-        </header>
+        {this.customRender()}
       </div>
     );
   }
